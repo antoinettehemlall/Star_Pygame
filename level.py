f91fa8608b9f7be242384f820fd1503a793d47a3
antoinettehemlall <hemlall3000@gmail.com>
2023-01-14 08:52:12 -0500

    Add level file

---
+++ level.py
@@ -0,0 +1,12 @@
+import pygame
+from settings import *
+
+class Level:
+    def __init__(self):
+
+        #get the display surface
+        self.display_surface = pygame.display.get_surface()
+
+        # sprite groups
+        self.all_sprites = pygame.sprite.Group()
+    def run(self, dt):
--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import pygame, sys
 from settings import *
+from level import Level
 
 class Game:
     def __init__(self):
         pygame.init()
         self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        pygame.display.set_caption('Star Game')
         self.clock = pygame.time.Clock()
+        self.level = Level()
 
     def run(self):
         while True:
@@ -14,7 +17,8 @@ class Game:
                     pygame.quit()
                     sys.exit()
 
-            dt =self.clock.tick() / 1000
+            dt = self.clock.tick() / 1000
+            self.level.run(dt)
             pygame.display.update()
 
 if __name__ == '__main__':

e7b7a79994e4b41dc80d27e6f4c274069c5862fe
antoinettehemlall <hemlall3000@gmail.com>
2023-01-13 20:05:09 -0500

    Initialize settings

--- settings.py
+++ settings.py
@@ -18,6 +18,36 @@ PLAYER_TOOL_OFFSET = {
 
 LAYERS = {
     'water': 0,
-    'ground'
-    'soil'
+    'ground': 1,
+    'soil': 2,
+    'soil water': 3,
+    'rain floor': 4,
+    'house bottom' : 5,
+    'ground plant' : 6,
+    'main': 7,
+    'house top': 8,
+    'fruit': 9,
+    'rain drops': 10
+}
+
+APPLE_POS = {
+    'Small': [(18, 17), (30,37), (12,50), (30,45), (20,30), (30, 10)],
+    'Large': [(30, 24), (60, 65), (50, 50), (16, 40), (45,50), (42,70)]
+}
+
+GROW_SPEED = {
+    'corn': 1,
+    'tomato': 0.7
+}
+
+SALE_PRICES = {
+    'wood': 4,
+    'apple': 2,
+    'corn' : 10,
+    'tomato' : 20,
+}
+
+PURCHASE_PRICES = {
+    'corn':4,
+    'tomato':5
 }
